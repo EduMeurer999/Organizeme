b235675ceb01256ee2536dcbbccb4fae4fe73bd2
Eduardo Meurer <eduardomeurre999@gmail.com>
2018-08-14 21:11:43 -0300

    Inicio do sistema de cadastro com sqlite

---
+++ app/src/main/java/com/example/a20151inf0182/organizeme/Database.java
@@ -0,0 +1,38 @@
+package com.example.a20151inf0182.organizeme;
+
+import android.content.Context;
+import android.database.SQLException;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Odair on 14/08/2018.
+ */
+
+public class Database extends SQLiteOpenHelper {
+    public static String NOME_BANCO;
+    public static String SQL;
+    public static Context CONTEXT;
+    public Database(Context context, String nomeBanco, String sql){
+        super(context, nomeBanco, null, 1);
+        this.NOME_BANCO = nomeBanco;
+        this.SQL = sql;
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        try {
+            db.execSQL(SQL);
+        }catch (SQLException e) {
+            Toast.makeText(CONTEXT, "Erro ao criar Tabela", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
--- app/src/main/java/com/example/a20151inf0182/organizeme/PostContract.java
+++
@@ -1,8 +0,0 @@
-package com.example.a20151inf0182.organizeme;
-
-/**
- * Created by Odair on 14/08/2018.
- */
-
-public final class PostContract {
-}
