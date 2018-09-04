95b0fa4043cc7d8735b7566ec2f7a7329e3d6d69
João <gabrielhartmannqwert@gmail.com>
2018-09-04 08:19:04 -0300

    login no firebase tela de perfil dentro de activity

--- app/src/main/java/com/example/a20151inf0182/organizeme/Activity/MainActivity.java
+++ app/src/main/java/com/example/a20151inf0182/organizeme/Activity/MainActivity.java
@@ -1,26 +1,55 @@
 package com.example.a20151inf0182.organizeme.Activity;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.example.a20151inf0182.organizeme.DAO.ConfiguracaoFirebase;
+import com.example.a20151inf0182.organizeme.Entidades.Usuarios;
 import com.example.a20151inf0182.organizeme.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
 
-    private EditText etdEmail;
-    private EditText etdSenha;
+    private EditText edtEmail;
+    private EditText edtSenha;
     private Button btnLogar;
-
+    private FirebaseAuth autenticacao;
+    private Usuarios usuarios;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //fazer a tela de login video aula 3
+        edtEmail = (EditText) findViewById(R.id.edtEmail);
+        edtSenha = (EditText) findViewById(R.id.edtSenha);
+        btnLogar = (Button) findViewById(R.id.btnLogar);
+
+        btnLogar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(!edtEmail.getText().toString().equals("") && (!edtSenha.getText().toString().equals(""))){
+                    usuarios = new Usuarios();
+                    usuarios.setEmial(edtEmail.getText().toString());
+                    usuarios.setEmial(edtSenha.getText().toString());
+                    validarLogin();
+                }else{
+                    Toast.makeText(MainActivity.this, "Preencha os campos", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+
         TextView txtCadastro = findViewById(R.id.txtCadastro);
         txtCadastro.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -29,7 +58,23 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(telaCadastro);
             }
         });
-        //fazer a tela de login video aula 3
-        // edtEmail = (EditText) findViewById(R.id.ed)
+
+    }
+
+    private void validarLogin(){
+        autenticacao = ConfiguracaoFirebase.getFirebaseAutenticacao();
+        autenticacao.signInWithEmailAndPassword(usuarios.getEmial(), usuarios.getSenha()).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+                    abrirTelaPrincipal();
+                    Toast.makeText(MainActivity.this, "Login efetuado com sucesso", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+    public void abrirTelaPrincipal(){
+        Intent intentAbrirTelaPrincipal = new Intent(MainActivity.this, PerfilActivity.class);
+        startActivity(intentAbrirTelaPrincipal);
     }
 }
