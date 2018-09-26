7101591c9943041fb01f0b7fdddd3c9c85528a95
20151inf0182 <eduardomeurer999@gmail.com>
2018-09-26 10:53:22 -0300

    Modificação na lógica do login. Foi adicionado um sistema de pesquisa para todas as chaves do firebase

--- app/src/main/java/com/example/a20151inf0182/organizeme/Activity/PerfilActivity.java
+++ app/src/main/java/com/example/a20151inf0182/organizeme/Activity/PerfilActivity.java
@@ -10,6 +10,7 @@ import android.widget.Button;
 
 import android.widget.TextView;
 import android.widget.Toast;
+import android.widget.ViewSwitcher;
 
 
 import com.example.a20151inf0182.organizeme.DAO.ConfiguracaoFirebase;
@@ -42,13 +43,14 @@ public class PerfilActivity extends AppCompatActivity {
         Intent i = getIntent();
         Usuarios usuario = (Usuarios) i.getSerializableExtra("Usuario");
 
-        Toast.makeText(this, ""+usuario.getId(), Toast.LENGTH_SHORT).show();
         final TextView tvNome = (TextView) findViewById(R.id.txtNome);
         final TextView tvIdade = (TextView) findViewById(R.id.txtIdade);
         final TextView tvCurso = (TextView) findViewById(R.id.txtCurso);
         final TextView tvEmail = (TextView) findViewById(R.id.txtEmail);
         final TextView tvSerie = (TextView) findViewById(R.id.txtSerie);
 
+        tvNome.setText("Nome: "+usuario.getNome());
+        tvEmail.setText("Email: "+usuario.getEmail());
 
 
         Button btnLogout = (Button) findViewById(R.id.btnLogout);
@@ -63,7 +65,7 @@ public class PerfilActivity extends AppCompatActivity {
 
 
     }
-    /*public void btnEditarAction(){
+    public void btnEditarAction(){
         btnEditar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -73,7 +75,7 @@ public class PerfilActivity extends AppCompatActivity {
                 myTV.setText("value");
             }
         });
-    }*/
+    }
 
 
 }
