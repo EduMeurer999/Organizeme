228560fcd37caa65de268d3cc6d3f4992e4c52aa
João <gabrielhartmannqwert@gmail.com>
2018-09-27 10:55:18 -0300

    teste do menu 3

--- app/src/main/java/com/example/a20151inf0182/organizeme/Activity/MainActivity.java
+++ app/src/main/java/com/example/a20151inf0182/organizeme/Activity/MainActivity.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -86,6 +88,23 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+    }
+
+
+    public boolean onCreateOptionMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        int id = item.getItemId();
+        if(id==R.id.opcPerfil){
+            Intent perfil=new Intent(MainActivity.this, PerfilActivity.class);
+            startActivity(perfil);
+            finish();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
 
