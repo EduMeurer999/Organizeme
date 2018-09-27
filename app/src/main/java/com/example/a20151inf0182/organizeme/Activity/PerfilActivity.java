ccffab1a3cbbca530fd07dd2b2956910ca71af67
João G <gabrielhartmannqwert@gmail.com>
2018-09-27 12:08:42 -0300

    teste do menu 4

--- app/src/main/java/com/example/a20151inf0182/organizeme/Activity/PerfilActivity.java
+++ app/src/main/java/com/example/a20151inf0182/organizeme/Activity/PerfilActivity.java
@@ -2,9 +2,15 @@ package com.example.a20151inf0182.organizeme.Activity;
 
 import android.content.Intent;
 
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
@@ -38,6 +44,7 @@ public class PerfilActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_perfil);
+
 //
         mDatabase = ConfiguracaoFirebase.getDatabaseReference();
         mAuth = ConfiguracaoFirebase.getFirebaseAuth();
@@ -73,6 +80,19 @@ public class PerfilActivity extends AppCompatActivity {
         });
 
 
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        return super.onOptionsItemSelected(item);
     }
 //    public void btnEditarAction(){
 //        btnEditar.setOnClickListener(new View.OnClickListener() {
