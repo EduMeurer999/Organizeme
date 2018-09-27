0f3886b892236970e24effc2c1299fd0dd99a507
Eduardo Meurer <eduardomeurre999@gmail.com>
2018-09-27 08:07:37 -0300

    Erro firebase

--- app/src/main/java/com/example/a20151inf0182/organizeme/Activity/SplashActivity.java
+++ app/src/main/java/com/example/a20151inf0182/organizeme/Activity/SplashActivity.java
@@ -8,18 +8,18 @@ import android.os.Bundle;
 import com.example.a20151inf0182.organizeme.R;
 
 public class SplashActivity extends AppCompatActivity {
-    private static int SPLASH_TIME_OUT=3000;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                Intent MainActivity=new Intent(SplashActivity.this, MainActivity.class);
-                startActivity(MainActivity);
-                finish();
-            }//fim run
-        }, SPLASH_TIME_OUT);
+//        new Handler().postDelayed(new Runnable() {
+//            @Override
+//            public void run() {
+//                Intent main=new Intent(SplashActivity.this, MainActivity.class);
+//                startActivity(main);
+//                finish();
+//            }//fim run
+//        }, SPLASH_TIME_OUT);
     }
 }
