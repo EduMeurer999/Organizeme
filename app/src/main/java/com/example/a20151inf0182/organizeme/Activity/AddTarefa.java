f8162cc12de6af8d1ac8a8798fa41c213d54b777
João G <gabrielhartmannqwert@gmail.com>
2018-09-27 23:50:41 -0300

    menu funcional e actionbar na cor mais certa, o "i.putExtra("Usuario", usuario);" ficou em tds as activitys, cada uma está isolado (criar classe unica).

--- app/src/main/java/com/example/a20151inf0182/organizeme/Activity/AddTarefa.java
+++ app/src/main/java/com/example/a20151inf0182/organizeme/Activity/AddTarefa.java
@@ -4,12 +4,16 @@ package com.example.a20151inf0182.organizeme.Activity;
 import android.app.Dialog;
 import android.content.DialogInterface;
 
+import android.content.Intent;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -33,6 +37,7 @@ public class AddTarefa extends AppCompatActivity {
     private FirebaseUser usuarioConectado;
     private DatabaseReference Database;
     private Usuarios usuario;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(final Bundle savedInstanceState) {
@@ -135,6 +140,52 @@ public class AddTarefa extends AppCompatActivity {
         return builder.create();
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.opcPerfil:
+                Intent i  = new Intent(this, PerfilActivity.class);
+                i.putExtra("Usuario", usuario);
+                startActivity(i);
+                break;
+            case R.id.opcAddTarefa:
+                i = new Intent(this, AddTarefa.class);
+                i.putExtra("Usuario", usuario);
+                startActivity(i);
+                break;
+            case R.id.opcNotas:
+                i = new Intent(this, NotasActivity.class);
+                i.putExtra("Usuario", usuario);
+                startActivity(i);
+                break;
+            case R.id.opcVerTarefas:
+                i = new Intent(this, TarefasActivity.class);
+                i.putExtra("Usuario", usuario);
+                startActivity(i);
+                break;
+            case R.id.opcAjuda:
+                i = new Intent(this, AjudaActivity.class);
+                i.putExtra("Usuario", usuario);
+                startActivity(i);
+                break;
+            case R.id.opcSair:
+                mAuth.signOut();
+                startActivity(new Intent(this, MainActivity.class));
+                break;
+
+        }
+        return super.onOptionsItemSelected(item);
+
+
+
+    }
 
 }
 
--- app/src/main/java/com/example/a20151inf0182/organizeme/Activity/tarefaActivity.java
+++ app/src/main/java/com/example/a20151inf0182/organizeme/Activity/tarefaActivity.java
@@ -1,16 +1,80 @@
 package com.example.a20151inf0182.organizeme.Activity;
 
+
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+
+import com.example.a20151inf0182.organizeme.DAO.ConfiguracaoFirebase;
+import com.example.a20151inf0182.organizeme.Entidades.Usuarios;
 import com.example.a20151inf0182.organizeme.R;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+
+import java.util.ArrayList;
 
 public class tarefaActivity extends AppCompatActivity {
 
+    private Usuarios usuario;
+    private FirebaseAuth mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tarefa);
         //teste
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.opcPerfil:
+                Intent i  = new Intent(this, PerfilActivity.class);
+                i.putExtra("Usuario", usuario);
+                startActivity(i);
+                break;
+            case R.id.opcAddTarefa:
+                i = new Intent(this, AddTarefa.class);
+                i.putExtra("Usuario", usuario);
+                startActivity(i);
+                break;
+            case R.id.opcNotas:
+                i = new Intent(this, NotasActivity.class);
+                i.putExtra("Usuario", usuario);
+                startActivity(i);
+                break;
+            case R.id.opcVerTarefas:
+                i = new Intent(this, TarefasActivity.class);
+                i.putExtra("Usuario", usuario);
+                startActivity(i);
+                break;
+            case R.id.opcAjuda:
+                i = new Intent(this, AjudaActivity.class);
+                i.putExtra("Usuario", usuario);
+                startActivity(i);
+                break;
+            case R.id.opcSair:
+                mAuth.signOut();
+                startActivity(new Intent(this, MainActivity.class));
+                break;
+
+        }
+        return super.onOptionsItemSelected(item);
+
+
+
     }
 }
